ef883849b544432048fc10c63718a25b76b98b18
salahxdk <salahxdk22@gmail.com>
2023-08-01 00:18:58 -0400

    this commit message will go through

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function StringDisplayApp() {
+  const [fact, setFact] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://catfact.ninja/fact');
+      setFact(response.data.fact);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>String Display App</h1>
+      <button onClick={fetchData}>Fetch String</button>
+      <textarea
+        readOnly
+        rows={5}
+        cols={50}
+        value={fact}
+        style={{ marginTop: '10px' }}
+      />
     </div>
   );
 }
 
-export default App;
+export default StringDisplayApp;

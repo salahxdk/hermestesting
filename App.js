1321a442dc4a9a822d8259bc9efa17f809e6ee2d
salahxdk <salahxdk22@gmail.com>
2023-08-01 09:33:25 -0400

    Updates

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function StringDisplayApp() {
+  const [fact, setFact] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://catfact.ninja/fact');
+      setFact(response.data.fact);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>String Display App</h1>
+      <button onClick={fetchData}>Fetch Sting</button>
+      <textarea
+        readOnly
+        rows={5}
+        cols={50}
+        value={fact}
+        style={{ marginTop: '10px' }}
+      />
+    </div>
+  );
+}
+
+export default StringDisplayApp;
